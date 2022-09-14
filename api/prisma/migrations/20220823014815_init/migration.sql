f8ee182e4052a0d5dba12065f734fe29888b7193
ItsKiri <99428690+ItsKiri@users.noreply.github.com>
2022-09-14 10:22:39 -0700

    Add files via upload

---
+++ api/prisma/migrations/20220823014815_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `confition` on the `TodoItem` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `TodoItem` DROP COLUMN `confition`;
