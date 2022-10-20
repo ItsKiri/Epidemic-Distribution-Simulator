e06c9f0b870219fa2056ccd917615c102e6a8d83
ItsKiri <99428690+ItsKiri@users.noreply.github.com>
2022-10-19 18:04:17 -0700

    Delete api directory

--- api/prisma/migrations/20220823014815_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `confition` on the `TodoItem` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `TodoItem` DROP COLUMN `confition`;
